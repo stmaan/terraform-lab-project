42397daf1212f892b50f9af359163c3629aa7f28
stmaan <learn.stmaan@mail.ru>
2026-05-06 12:41:01 +0000

    feat: app fully configured with env variables, module 1 complete

--- app.tf
+++ app.tf
@@ -2,34 +2,26 @@ resource "kubernetes_deployment" "flask_app" {
   metadata {
     name      = "educational-app"
     namespace = kubernetes_namespace.terraform_lab.metadata.0.name
-    labels = {
-      app = "educational-app"
-    }
+    labels    = { app = "educational-app" }
   }
 
   spec {
     replicas = 2
-    selector {
-      match_labels = {
-        app = "educational-app"
-      }
-    }
+    selector { match_labels = { app = "educational-app" } }
     template {
-      metadata {
-        labels = {
-          app = "educational-app"
-        }
-      }
+      metadata { labels = { app = "educational-app" } }
       spec {
         container {
-          # Используем ТОЧНОЕ имя и ТОЧНЫЙ хеш из вывода ctr
           image = "stmaan/educationalcicd@sha256:14e8fe8b662a04ab57d64412998c29893f24e6476cb32e3bc9ee55167a4108a8"
           name  = "flask-container"
-          
           image_pull_policy = "IfNotPresent"
+	env {
+            name  = "APP_ENV"
+            value = "Terraform-Lab"
+          }
 
           port {
-            container_port = 5000
+            container_port = 80 # <-- ИСПРАВЛЕНО (было 5000)
           }
         }
       }
@@ -43,12 +35,10 @@ resource "kubernetes_service" "flask_service" {
     namespace = kubernetes_namespace.terraform_lab.metadata.0.name
   }
   spec {
-    selector = {
-      app = "educational-app"
-    }
+    selector = { app = "educational-app" }
     port {
       port        = 80
-      target_port = 5000
+      target_port = 80 # <-- ИСПРАВЛЕНО (было 5000)
       node_port   = 30085
     }
     type = "NodePort"
